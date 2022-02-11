5d0d0d3daf0f2cb1d659603bcba4f1026aed95e6
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-02-11 13:48:38 +0000

    arror functions

--- arrorfunctions.js
+++ arrorfunctions.js
@@ -1,11 +1,11 @@
 //function without return keyword
 const addTwoNumbers = (a, b) => a + b;
-let sum2 = addTwoNumbers (2, 4);
+let sum2 = addTwoNumbers(2, 4);
 console.log(sum2)
 
 //implicit returns, single parameter no need for parentheses
 const saySomething = message => console.log(message);
-saySomething ("hello there!!");
+saySomething("hello there!!");
 
 //no parameter
 const sayHello = () => console.log('hello');
@@ -13,8 +13,9 @@ sayHello();
 console.log(sum2)
 
 const complexNumbers = (a, b) => {
-const sum = a + b;
-return sum* 500}
+    const sum = a + b;
+    return sum * 500
+}
 
-let num= complexNumbers (2, 2);
+let num = complexNumbers(2, 2);
 console.log(num);
