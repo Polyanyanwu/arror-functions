439acaf95a0a4e90732e3ff62596e495456f9e5f
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-03-03 14:14:30 +0000

    format plindrome function

--- python_functions.py
+++ python_functions.py
@@ -3,13 +3,16 @@ Function to check if a word reversed is same as original word
 Palindrome. ignore spaces
 """
 
-def isPalindrome(str_val):
-    if len(str_val) > 0:
-        str_val = str_val.replace(" ", "") #empty strings
-        revString = str_val[::-1]
-        if str_val.lower() == revString.lower():
-            return True
-    return False   
 
-print(isPalindrome('carrace'))
-print(isPalindrome('ra cec  ar'))
+def is_palindrome(str_val):
+    """ determine if a word is a palindrome """
+    if len(str_val) > 0:
+        str_val = str_val.replace(" ", "")  # empty strings
+        rev_string = str_val[::-1]
+        if str_val.lower() == rev_string.lower():
+            return True
+    return False
+
+
+print(is_palindrome('carrace'))
+print(is_palindrome('ra cec  ar'))
